cbfcc7e89ed241266a7286ae20ea2e584e35a437
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-07 14:07:14 +0900

    ユーザー管理機能の実装準備

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,16 @@
+class PrototypesController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  #def index
+  #end
+  #def index
+  #end
+  #def index
+  #end
+end
