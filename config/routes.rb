4172f4637b531939a019434d41e14fca3063966f
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-10 16:21:02 +0900

    エラーの解除

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    #root "articles#index"
     root to: "prototypes#index"
-    resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-    resources :users
+    resources :prototypes
 end
